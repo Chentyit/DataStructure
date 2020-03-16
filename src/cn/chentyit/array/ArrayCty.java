e506b5088b3f994f56da73f3584585eca2d29925
chentyit <chentyit@qq.com>
2020-03-16 15:56:14 +0800

    【数据结构复习】数组中添加元素

--- src/cn/chentyit/array/ArrayCty.java
+++ src/cn/chentyit/array/ArrayCty.java
@@ -17,7 +17,7 @@ public class ArrayCty {
      */
     public ArrayCty(int capacity) {
         this.data = new int[capacity];
-        size = 0;
+        this.size = 0;
     }
 
     /**
@@ -33,7 +33,7 @@ public class ArrayCty {
      * @return
      */
     public int getSize() {
-        return size;
+        return this.size;
     }
 
     /**
@@ -42,6 +42,55 @@ public class ArrayCty {
      * @return
      */
     public int getCapacity() {
-        return data.length;
+        return this.data.length;
+    }
+
+    /**
+     * 返回数组是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    /**
+     * 向所有元素后添加一个新元素
+     *
+     * @param e
+     */
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    /**
+     * 在所有元素前添加一个元素
+     *
+     * @param e
+     */
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+    /**
+     * 在第 index 位置插入一个新的元素 e
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, int e) {
+        if (this.size == data.length) {
+            throw new IllegalArgumentException("AddLast failed. Array is full");
+        }
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("AddLast failed. Require index >= 0 and index <= size");
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
     }
 }
